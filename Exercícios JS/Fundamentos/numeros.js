df0c0aefd3d54a2d0958eb1332da65bd92702593
Marlon Jesus Rodrigues de Pinho <marlonjesus88@gmail.com>
2021-01-09 19:03:17 -0300

    Atividade
    algumas funções para tomar cuidado!

--- Exercícios JS/Fundamentos/numeros.js
+++ Exercícios JS/Fundamentos/numeros.js
@@ -12,3 +12,6 @@ const total = avaliacao1 * peso1 + avaliacao2 * peso2
 const media = total / (peso1 + peso2)
 
 console.log(media.toFixed(2))
+console.log(media.toString(2)) // em binário
+console.log(typeof media)
+console.log(typeof Number)
