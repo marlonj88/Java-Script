62e9c182076258ee5d10285e542a507c991e783a
Marlon Jesus Rodrigues de Pinho <marlonjesus88@gmail.com>
2021-01-25 12:54:11 -0300

    Aulas 22 a 32
    aulas assistidas.

---
+++ usandoVar1.js
@@ -0,0 +1,20 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+// console.log(local) // Erro! Não é possivel acessar uma variavel fora da funcao₢
